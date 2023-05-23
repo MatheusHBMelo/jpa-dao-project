a199826eda427a4fdf9f231fb8801cdd07580681
MatheusHBMelo <matheushbmelo@gmail.com>
2023-05-23 14:59:08 -0300

    [STYLE] Update in style of the class

--- src/main/java/com/theuz/model/dao/DaoFactory.java
+++ src/main/java/com/theuz/model/dao/DaoFactory.java
@@ -5,10 +5,11 @@ import com.theuz.model.dao.impl.PilotoDaoImpl;
 import com.theuz.model.db.DB;
 
 public class DaoFactory {
-    public static EquipeDao createEquipe(){
+    public static EquipeDao createEquipe() {
         return new EquipeDaoImpl(DB.getConnection());
     }
-    public static PilotoDao createPiloto(){
-     return new PilotoDaoImpl(DB.getConnection());
+
+    public static PilotoDao createPiloto() {
+        return new PilotoDaoImpl(DB.getConnection());
     }
 }
