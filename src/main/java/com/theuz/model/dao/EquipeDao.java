dcbd0d2345d246fea6ec356b25c440cf823285e7
MatheusHBMelo <matheushbmelo@gmail.com>
2023-05-23 11:31:38 -0300

    [FEAT] Created Dao interface to Equipe

---
+++ src/main/java/com/theuz/model/dao/EquipeDao.java
@@ -0,0 +1,13 @@
+package com.theuz.model.dao;
+
+import com.theuz.model.entities.Equipe;
+
+import java.util.List;
+
+public interface EquipeDao {
+    void insert(Equipe equipe);
+    void update(Equipe equipe);
+    void remove(Integer id);
+    Equipe findById(Integer id);
+    List<Equipe> findAll();
+}
