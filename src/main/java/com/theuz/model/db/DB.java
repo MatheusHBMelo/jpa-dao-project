0c4b0b1c9812ad7e1d02a19ac29602f060220f08
MatheusHBMelo <matheushbmelo@gmail.com>
2023-05-23 11:30:40 -0300

    [FEAT] Add Connection DB class

---
+++ src/main/java/com/theuz/model/db/DB.java
@@ -0,0 +1,25 @@
+package com.theuz.model.db;
+
+import com.theuz.model.db.exceptions.DbException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DB {
+    private static EntityManagerFactory emf = null;
+    private static EntityManager em = null;
+
+    public static EntityManager getConnection(){
+        if (em == null){
+            try {
+                emf = Persistence.createEntityManagerFactory("jpa-test-unit");
+                EntityManager em = emf.createEntityManager();
+                return em;
+            } catch (RuntimeException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+        return em;
+    }
+}
