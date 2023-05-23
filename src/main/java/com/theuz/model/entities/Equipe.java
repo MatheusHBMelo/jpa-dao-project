cc152b5b27ce0c0d99a510f448f78917db3459e2
MatheusHBMelo <matheushbmelo@gmail.com>
2023-05-23 09:49:48 -0300

    [FEAT] Created equipe entity

---
+++ src/main/java/com/theuz/model/entities/Equipe.java
@@ -0,0 +1,92 @@
+package com.theuz.model.entities;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "equipe")
+public class Equipe implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_equipe")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_fundacao")
+    private Date dataFundacao;
+
+    @OneToMany(mappedBy = "equipe")
+    private List<Piloto> pilotos;
+
+    public Equipe() {
+    }
+
+    public Equipe(Integer id, String name, Date dataFundacao) {
+        this.id = id;
+        this.name = name;
+        this.dataFundacao = dataFundacao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(Date dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    public List<Piloto> getPilotos() {
+        return pilotos;
+    }
+
+    public void setPilotos(List<Piloto> pilotos) {
+        this.pilotos = pilotos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Equipe equipe = (Equipe) o;
+        return Objects.equals(id, equipe.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                " - Name: " + name +
+                " - Data de fundação: " + dataFundacao +
+                " - Pilotos: " + pilotos;
+    }
+}
